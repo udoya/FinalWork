087444648965657d87b8cc7ad617b7783fbf6e80
yakisoba <nakajima@keio.jp>
2022-07-16 16:41:08 +0900

    パッケージ内からしかアクセスできないように

--- src/Product/Product.java
+++ src/Product/Product.java
@@ -10,11 +10,11 @@ import java.util.TreeMap;
  * - numLendingTreeMap: Tree of the information that *who* is borrowing *how many* products
 //  * - price: price of the product
  */
-public class Product {
-    protected String name;
-    protected int numTotal;
-    protected int numAvailable;
-    protected TreeMap<Integer, Integer> numLendingTreeMap = new TreeMap<Integer, Integer>();
+class Product {
+    String name;
+    int numTotal;
+    int numAvailable;
+    TreeMap<Integer, Integer> numLendingTreeMap = new TreeMap<Integer, Integer>();
     /* NOTE: the key is ID who borrows, the value is number of borrowing */
 
     /* Constructor */
@@ -23,7 +23,7 @@ public class Product {
      * @param name the name of the product
      * @param numTotal the number of the products
      */
-    public Product(String name, int numTotal) {
+    Product(String name, int numTotal) {
         this.name = name;
         this.numTotal = numTotal;
         this.numAvailable = numTotal;
@@ -31,54 +31,76 @@ public class Product {
         System.out.println("Product: " + name + " has been added as " + numAvailable + ".");
     }
 
-    /* For Staff */
-    /** 製品名変更
-     * @param name: the name of the product
-     */
-    // TODO: What if the name is already in the list? Maybe it should be in ProductSystem?
-    void changeName(String name) {
-        String tmp = this.name;
-        this.name = name;
-        // TODO: Print on the console the following message:
-        System.out.println("Product name changed to " + name + " from " + tmp + ".");
-    }
+    /* ProductSystemからしかアクセスできないようにしちゃっていいかもね */
+    // String getName() {
+    //     return name;
+    // }
+    // int getNumTotal() {
+    //     return numTotal;
+    // }
+    // int getNumAvailable() {
+    //     return numAvailable;
+    // }
 
-    /* For Staff */
-    /** 在庫追加・削除
-     * @param difNum: amount of change
-     */
-    void changenumAvailable(int difNum) {
-        if (this.numAvailable + difNum >= 0) {
-            this.numAvailable += difNum;
-        } else {
-            // TODO: error;
-        }
-    }
+    // TreeMap<Integer, Integer> getLendingList() {
+    //     return numLendingTreeMap;
+    // }
+    // int getNumLending() {
+    //     return numTotal - numAvailable;
+    // }
+    // int getNumLending(int id) {
+    //     return numLendingTreeMap.get(id);
+    // }
 
-    /* For Custormer */
-    /** Use when a customer borrows
-     * @param id: the ID of the customer
-     * @param num: the number of the products that are being borrowed
-     */
-    void borrowThis(int id, int num) {
-        if (this.numAvailable >= num) {
-            this.numAvailable -= num;
-            if (this.numLendingTreeMap.containsKey(id)) {
-                this.numLendingTreeMap.put(id, this.numLendingTreeMap.get(id) + num);
-            } else {
-                this.numLendingTreeMap.put(id, num);
-            }
-        }
-    }
+    /* 全部ProductSystemでいいかもです */
+    // /* For Staff */
+    // /** 製品名変更
+    //  * @param name: the name of the product
+    //  */
+    // // TODO: What if the name is already in the list? Maybe it should be in ProductSystem?
+    // void changeName(String name) {
+    //     String tmp = this.name;
+    //     this.name = name;
+    //     // TODO: Print on the console the following message:
+    //     System.out.println("Product name changed to " + name + " from " + tmp + ".");
+    // }
 
-    void returnThis(int id, int num) {
-        if (this.numLendingTreeMap.containsKey(id)) {
-            if (this.numLendingTreeMap.get(id) >= num) {
-                this.numLendingTreeMap.put(id, this.numLendingTreeMap.get(id) - num);
-                this.numAvailable += num;
-            } else {
+    // /* For Staff */
+    // /** 在庫追加・削除
+    //  * @param difNum: amount of change
+    //  */
+    // void changenumAvailable(int difNum) {
+    //     if (this.numAvailable + difNum >= 0) {
+    //         this.numAvailable += difNum;
+    //     } else {
+    //         // TODO: error;
+    //     }
+    // }
+
+    // /* For Custormer */
+    // /** Use when a customer borrows
+    //  * @param id: the ID of the customer
+    //  * @param num: the number of the products that are being borrowed
+    //  */
+    // void borrowThis(int id, int num) {
+    //     if (this.numAvailable >= num) {
+    //         this.numAvailable -= num;
+    //         if (this.numLendingTreeMap.containsKey(id)) {
+    //             this.numLendingTreeMap.put(id, this.numLendingTreeMap.get(id) + num);
+    //         } else {
+    //             this.numLendingTreeMap.put(id, num);
+    //         }
+    //     }
+    // }
+
+    // void returnThis(int id, int num) {
+    //     if (this.numLendingTreeMap.containsKey(id)) {
+    //         if (this.numLendingTreeMap.get(id) >= num) {
+    //             this.numLendingTreeMap.put(id, this.numLendingTreeMap.get(id) - num);
+    //             this.numAvailable += num;
+    //         } else {
                 
-            }
-        }
-    }
+    //         }
+    //     }
+    // }
 }
