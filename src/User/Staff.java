5cb487f7175ba6e662f420236ea7881ca1446892
yakisoba <nakajima@keio.jp>
2022-07-18 14:54:09 +0900

    オブジェクト指向を完全に理解したメソッド整理

--- src/User/Staff.java
+++ src/User/Staff.java
@@ -1,39 +1,10 @@
 package User;
 
-import java.util.*;
-import Product.*;
-
 public class Staff extends User {
 
     public Staff(String name, int id, String password) {
         super(name, id, password);
         isMaster = true;
-    }
-
-    HashMap<String, Integer> getBorrowingList(int customerId) {
-        HashMap<String, Integer> borrowingList = new HashMap<String, Integer>();
-
-        return borrowingList;
-    }
-
-    HashMap<String, Integer> getLendingList() {
-        return borrowingList;
-    }
-
-    // show all information of all furnitures in the list
-    public getProductList() {
-        return productList;
-    }
-
-    // partial match search
-    public ArrayList<Product> getProductList(String name) {
-        ArrayList<Product> productList = new ArrayList<Product>();
-        for (int i = 0; i < this.productList.size(); i++) {
-            if (this.productList.get(i).name.indexOf(name) != -1) {
-                productList.add(this.productList.get(i));
-            }
-        }
-        return productList;
     }
 
 }
--- src/User/User.java
+++ src/User/User.java
@@ -3,8 +3,8 @@ package User;
 abstract class User {
     protected String name;
     protected int id;
-    protected boolean isMaster = false;
     protected String password;
+    protected boolean isMaster = false;
 
     // For Sign in
     // Constructor
@@ -12,5 +12,37 @@ abstract class User {
         this.name = name;
         this.id = id;
         this.password = password;
+    }
+
+    // Setter
+    public void changeName(String name) {
+        this.name = name;
+    }
+    public void changeId(int id) {
+        this.id = id;
+    }
+    public void changeIsMaster(boolean isMaster) {
+        this.isMaster = isMaster;
+    }
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+    public int getId() {
+        return id;
+    }
+    public boolean isMaster() {
+        return isMaster;
+    }
+    public String getPassword(int id) {
+        if (this.id == id) {
+            return password;
+        } else {
+            return "";
+        }
     }
 }
