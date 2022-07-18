59889aa88813431193706f36f312d4fee7030ac9
yakisoba <nakajima@keio.jp>
2022-07-18 14:53:41 +0900

    Mainこんな形かな

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,20 @@
 import Product.*;
 
 public class Main {
+    static ProductSystem productSystem = new ProductSystem();
 
     // NOTE: Create Sample Data (to make another Class?)
 
     public static void main(String[] args) {
         // WorkFlow
 
-        ProductSystem pList = new ProductSystem();
-
         try {
-            pList.addProduct(new Product("Wooden Chair", 10));
-            pList.addProduct(new Product("Wooden Table", 15));
-            pList.addProduct(new Product("White Wooden Chair", 20));
-            pList.addProduct(new Product("White Wooden Table", 25));
-            pList.addProduct(new Product("Black Wooden Chair", 10));
-            pList.addProduct(new Product("Black Wooden Table", 35));
+            productSystem.addProduct(new Product("Wooden Chair", 10));
+            productSystem.addProduct(new Product("Wooden Table", 15));
+            productSystem.addProduct(new Product("White Wooden Chair", 20));
+            productSystem.addProduct(new Product("White Wooden Table", 25));
+            productSystem.addProduct(new Product("Black Wooden Chair", 10));
+            productSystem.addProduct(new Product("Black Wooden Table", 35));
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
