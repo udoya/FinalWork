1b053445ff54896046946262e38f2b17cd19653b
yakisoba <nakajima@keio.jp>
2022-07-20 16:29:51 +0900

    Customerが追加できるようになりました～～～ とりあえず"Anonymous"って名前でSignUpできます

--- src/GUI/SignUpPanel.java
+++ src/GUI/SignUpPanel.java
@@ -2,6 +2,8 @@
 package GUI;
 
 import GUI.subPanel.FooterPanel;
+import GUI.*;
+import User.*;
 
 import java.awt.Color;
 import javax.swing.*;
@@ -11,20 +13,59 @@ import java.awt.event.*;
 public class SignUpPanel extends JPanel {
     private static final long serialVersionUID = 1L;
 
-    JTextField IDField;
-    JTextField passwordField;
+    JTextField nameField;
+    JTextField idField;
+    JTextField pwdField;
     JButton rtnBtn;
     JButton signUpBtn;
+
+    UserModel uModel = Main.uModel;
 
     class BtnAction implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == signUpBtn) {
-                // input from JtextField
-                String ID = IDField.getText();
-                String password = passwordField.getText();
-                // pop up a window to show the input
-                JOptionPane.showMessageDialog(null, "ID: " + ID + "\nPassword: " + password, "Sign In",
-                        JOptionPane.INFORMATION_MESSAGE);
+                // Get name
+                // String name = nameField.getText();
+                // TODO: Uncomment when nameField is ready
+
+                /* Test */
+                String name = "Anonymous";
+                /* End of test */
+
+                // care about empty field
+                if (name.equals("")) {
+                    JOptionPane.showMessageDialog(null, "Name is empty");
+                    return;
+                }
+
+                // Get ID
+                String ID = idField.getText();
+                // care about empty field
+                if (idField.getText().equals("")) {
+                    JOptionPane.showMessageDialog(null, "ID is empty");
+                    return;
+                }
+                // care about duplicate ID
+                if (uModel.getUser(ID) != null) {
+                    JOptionPane.showMessageDialog(null, "ID is duplicate");
+                    return;
+                }
+
+                // Get password
+                String pwd = pwdField.getText();
+                // care about empty field
+                if (pwd.equals("")) {
+                    JOptionPane.showMessageDialog(null, "Password is empty");
+                    return;
+                }
+
+                try {
+                    uModel.addUser(new Customer(name, ID, pwd));
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+                JOptionPane.showMessageDialog(null, "Welcome " + name + "!\nYou have successfully signed up.");
+                Main.mainWindow.setFrontScreenAndFocus(ScreenMode.LOGIN);
             }
             if (e.getSource() == rtnBtn) {
                 Main.mainWindow.setFrontScreenAndFocus(ScreenMode.LOGIN);
@@ -37,12 +78,13 @@ public class SignUpPanel extends JPanel {
         // this.setLayout(null);
     }
 
+    // TODO: add Username field
     public Component createMains() {
 
         // init Button and Field
         BtnAction btnListener = new BtnAction();
-        IDField = new JTextField(10);
-        passwordField = new JTextField(10);
+        idField = new JTextField(10);
+        pwdField = new JTextField(10);
         rtnBtn = new JButton("return to login");
         signUpBtn = new JButton("Sign Up");
 
@@ -58,10 +100,10 @@ public class SignUpPanel extends JPanel {
 
         // subP1 is 2*2
         subP1.setLayout(new GridLayout(2, 2));
-        subP1.add(new JLabel(" Username :", SwingConstants.LEFT));
-        subP1.add(IDField);
+        subP1.add(new JLabel(" UserID :", SwingConstants.LEFT));
+        subP1.add(idField);
         subP1.add(new JLabel(" Password :", SwingConstants.LEFT));
-        subP1.add(passwordField);
+        subP1.add(pwdField);
 
         // change the Rgs button's background color and size
         subP2.setLayout(new FlowLayout());
